0828a219b8859a5d0f2cdb42e19ce1bc5b99ac18
acandiach <abelcandiach@gmail.com>
2026-05-28 23:38:40 -0400

    saving commands surround

--- lua/abel/plugins/surround.lua
+++ lua/abel/plugins/surround.lua
@@ -3,15 +3,15 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   config = true,
-  opts = {
-      mappings = {
-        add = "sa",
-        delete = "sd",
-        find = "gsf",
-        find_left = "gsF",
-        highlight = "gsh",
-        replace = "gsr",
-        update_n_lines = "gsn",
-      },
-    },
 }
+
+--     Old text                    Command         New text
+-- --------------------------------------------------------------------------------
+--     surr*ound_words             ysiw)           (surround_words)
+--     surr*ound_words             ysiw(           ( surround_words )
+--     *make strings               ys$"            "make strings"
+--     [delete ar*ound me!]        ds]             delete around me!
+--     remove <b>HTML t*ags</b>    dst             remove HTML tags
+--     'change quot*es'            cs'"            "change quotes"
+--     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+--     delete(functi*on calls)     dsf             function calls
