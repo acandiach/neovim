2774c103fe73d8982d1a748b89138589ded191cf
Acandiach <abelcandiach@gmail.com>
2026-07-14 22:36:30 -0400

    agregando nuevo colorscheme

--- lua/abel/plugins/colorscheme.lua
+++ lua/abel/plugins/colorscheme.lua
@@ -14,14 +14,31 @@ return {
       -- colorscheme tokyonight-moon
 
 ------------------------------------------O-------------------------------------------------------------------------
-	"sainnhe/gruvbox-material",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-     vim.g.gruvbox_material_background = "hard"
-     vim.g.gruvbox_material_better_performance = 1
-     vim.g.gruvbox_material_transparent_background = 2 -- 0, 1 (only background), 2 (background and status line, etc.)
-     vim.cmd([[colorscheme gruvbox-material]])
-	end,
+	-- "sainnhe/gruvbox-material",
+	--  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	--  priority = 1000, -- make sure to load this before all the other start plugins
+	--  config = function()
+	--     vim.g.gruvbox_material_background = "hard"
+	--     vim.g.gruvbox_material_better_performance = 1
+	--     vim.g.gruvbox_material_transparent_background = 2 -- 0, 1 (only background), 2 (background and status line, etc.)
+	--     vim.cmd([[colorscheme gruvbox-material]])
+	-- end,
 
+  ----------------------------------------------O------------------------------------------------------------------
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function ()
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = true, -- disables setting the background color.
+      float = {
+          transparent = true, -- enable transparent floating windows
+          solid = false, -- use solid styling for floating windows, see |winborder|
+      },
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    })
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin-nvim"
+  end
 }
