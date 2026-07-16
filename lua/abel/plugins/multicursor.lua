fd9e22de77c138d559ffd5fae7a6d283c3046473
Acandiach <abelcandiach@gmail.com>
2026-07-16 13:43:07 -0400

    modificacion rezise y multicursor

--- lua/abel/plugins/multicursor.lua
+++ lua/abel/plugins/multicursor.lua
@@ -8,8 +8,8 @@ return {
     local set = vim.keymap.set
 
     -- Add or skip cursor above/below the main cursor.
-    set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
-    set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
+    set({"n", "x"}, "<A-up>", function() mc.lineAddCursor(-1) end)
+    set({"n", "x"}, "<A-down>", function() mc.lineAddCursor(1) end)
     set({"n", "x"}, "<leader><up>", function() mc.lineskipcursor(-1) end)
     set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
 
@@ -32,8 +32,8 @@ return {
     mc.addKeymapLayer(function(layerSet)
 
       -- Select a different cursor as the main one.
-      layerSet({"n", "x"}, "<left>", mc.prevCursor)
-      layerSet({"n", "x"}, "<right>", mc.nextCursor)
+      layerSet({"n", "x"}, "<A-left>", mc.prevCursor)
+      layerSet({"n", "x"}, "<A-right>", mc.nextCursor)
 
       -- Delete the main cursor.
       layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
